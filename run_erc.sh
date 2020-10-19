843c5c0c91649b52e6e3b310d619d9b971a0c9da
Pierre Chevalier <pierrechevalier83@gmail.com>
2020-10-19 15:20:03 +0100

    Try and fix issue with run_erc on ubuntu

--- run_erc.sh
+++ run_erc.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ERC_OUTPUT=$(docker run --rm -t -v "$(pwd)"/$1:/kicad-project productize/kicad-automation-scripts python -m kicad-automation.eeschema.schematic run_erc /kicad-project/ferris.sch "$(pwd)"/build | grep "DEBUG:root:Last line" | sed "s/DEBUG:root:Last line:  //")
 
 if [[ -z "$ERC_OUTPUT" ]]; then
@@ -5,7 +6,7 @@ if [[ -z "$ERC_OUTPUT" ]]; then
 	echo "Missing ERC report line. Something went wrong"
 	exit 2
 else
-	if [[ "$ERC_OUTPUT" = *"ERC messages: 0  Errors 0  Warnings 0"* ]]; then
+	if [[ "$ERC_OUTPUT"=="** ERC messages: 0  Errors 0  Warnings 0" ]]; then
 		echo -e "\e[1;32mPASS\e[0m"
 		echo "$ERC_OUTPUT"
 	    exit 0
