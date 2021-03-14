25a6cedca908636a78c720bd3cdf8c598403941d
Pierre Chevalier <pierrechevalier83@gmail.com>
2021-03-14 04:44:12 +0000

    0.2/bling: Add human readable "Part Name" field
    Use short human readable names to describe components, such as "MCU"
    instead of "STM32F072CBT6".
    
    This allows us to be quite precise in the "value" field while displaying
    an easy to understand description of each component in the generated
    interactive Bill of Material.
    
    We can then use the same language on the BOM cards for a smooth user
    experience of assembling one's Ferris.

--- 0.2/bling/ferris.pro
+++ 0.2/bling/ferris.pro
@@ -1,4 +1,4 @@
-update=Fri Sep 25 18:03:47 2020
+update=Sun 14 Mar 2021 05:28:43 GMT
 version=1
 last_client=kicad
 [general]
@@ -12,16 +12,6 @@ NetIExt=net
 version=1
 LibDir=
 [eeschema/libraries]
-[schematic_editor]
-version=1
-PageLayoutDescrFile=
-PlotDirectoryName=
-SubpartIdSeparator=0
-SubpartFirstId=65
-NetFmtName=Pcbnew
-SpiceAjustPassiveValues=0
-LabSize=50
-ERC_TestSimilarLabels=1
 [pcbnew]
 version=1
 PageLayoutDescrFile=
@@ -257,3 +247,13 @@ uViaDrill=0.1
 dPairWidth=0.2
 dPairGap=0.25
 dPairViaGap=0.25
+[schematic_editor]
+version=1
+PageLayoutDescrFile=
+PlotDirectoryName=
+SubpartIdSeparator=0
+SubpartFirstId=65
+NetFmtName=CadStar
+SpiceAjustPassiveValues=0
+LabSize=50
+ERC_TestSimilarLabels=1
--- run_ibom.sh
+++ run_ibom.sh
@@ -7,6 +7,6 @@ VARIANT=$1
 # Try 5 times as it sometimes fails
 for attempt in {0..5}
 do
-    docker run --user $(id -u $USER):$(id -g $USER) -e VARIANT=$VARIANT -v $WORKDIR:/workdir -t pierrechevalier83/ferris_automation bash -c 'echo ${VARIANT} && Xvfb :98 & python3 tools/InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py $VARIANT/ferris.kicad_pcb --dest-dir ../../build/$VARIANT --netlist-file $VARIANT/ferris.xml --extra-fields "LCSC Part" --dnp-field "DNP" --no-browser' && exit 0
+    docker run --user $(id -u $USER):$(id -g $USER) -e VARIANT=$VARIANT -v $WORKDIR:/workdir -t pierrechevalier83/ferris_automation bash -c 'echo ${VARIANT} && Xvfb :98 & python3 tools/InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py $VARIANT/ferris.kicad_pcb --dest-dir ../../build/$VARIANT --netlist-file $VARIANT/ferris.xml --extra-fields "LCSC Part,Part Name" --dnp-field "DNP" --no-browser' && exit 0
 done
 exit 1
